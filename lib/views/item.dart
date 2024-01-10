a613749c0e6081a66ed16fe07226ad2ce77cd007
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-10 11:20:44 +0100

    ModifiedM lib/views/lister.dart ?? lib/views/item.dart

---
+++ lib/views/item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:latext/latext.dart';
+import 'package:maxwell/utils/globals.dart';
+
+class Item extends StatefulWidget {
+  const Item({super.key, required this.item});
+  final Map<String, dynamic> item;
+  @override
+  State<Item> createState() => _ItemState();
+}
+
+class _ItemState extends State<Item> {
+  bool _isOpened = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: grey.withOpacity(.1), border: Border.all(color: grey, width: .5), borderRadius: BorderRadius.circular(15)),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Flexible(child: LaTexT(laTeXCode: Text(widget.item["title"], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: pink)))),
+                const SizedBox(height: 10),
+                Flexible(child: LaTexT(laTeXCode: Text(widget.item["expression"], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: white)))),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          IconButton(onPressed: () => setState(() => _isOpened = !_isOpened), icon: Icon(_isOpened ? Bootstrap.chevron_down : Bootstrap.chevron_left, size: 15, color: pink)),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/lister.dart
+++ lib/views/lister.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:maxwell/utils/globals.dart';
 import 'package:maxwell/views/empty.dart';
+import 'package:maxwell/views/item.dart';
 import 'package:maxwell/views/viewer.dart';
 
 class Lister extends StatelessWidget {
@@ -25,33 +26,9 @@ class Lister extends StatelessWidget {
               child: ListView.separated(
                 padding: const EdgeInsets.all(16),
                 physics: const BouncingScrollPhysics(),
-                itemBuilder: (BuildContext context, int index) => StatefulBuilder(
-                  builder: (BuildContext context, void Function(void Function()) _) {
-                    return GestureDetector(
-                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Viewer(expression: items[index]["expression"]))),
-                      child: Container(
-                        decoration: BoxDecoration(color: grey.withOpacity(.1), border: Border.all(color: grey, width: .5), borderRadius: BorderRadius.circular(15)),
-                        padding: const EdgeInsets.all(8),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Flexible(child: Text(items[index], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: pink))),
-                                  const SizedBox(height: 10),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            const Icon(Bootstrap.chevron_right, size: 15, color: pink),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
+                itemBuilder: (BuildContext context, int index) => GestureDetector(
+                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Viewer(expression: items[index]["expression"]))),
+                  child: Item(item: items[index]),
                 ),
                 separatorBuilder: (BuildContext context, int index) => Container(width: MediaQuery.sizeOf(context).width, height: .5, color: grey, margin: const EdgeInsets.symmetric(vertical: 8)),
                 itemCount: items.length,
