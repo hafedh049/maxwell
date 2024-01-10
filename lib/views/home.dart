6901ed24daeefc759132350aa176c2eb85cd1074
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-10 10:55:44 +0100

    ModifiedM lib/views/home.dart  M pubspec.yaml ?? lib/views/lister.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:maxwell/utils/globals.dart';
 import 'package:maxwell/utils/methods.dart';
 import 'package:maxwell/views/empty.dart';
@@ -16,34 +17,48 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: FutureBuilder<Map<String, dynamic>>(
-            future: load(),
-            builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
-              if (snapshot.hasData) {
-                if (snapshot.data!.isNotEmpty) {
-                  final Map<String, dynamic> data = snapshot.data!;
-                  return ListView.separated(
-                    itemBuilder: (BuildContext context, int index) => Container(
-                      decoration: BoxDecoration(
-                        color: grey.withOpacity(.1),
-                        border: Border.all(color: grey, width: .5),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: load(),
+        builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data!.isNotEmpty) {
+              final Map<String, dynamic> data = snapshot.data!;
+              return ListView.separated(
+                padding: const EdgeInsets.all(16),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) => GestureDetector(
+                  onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => )),
+                  child: Container(
+                    decoration: BoxDecoration(color: grey.withOpacity(.1), border: Border.all(color: grey, width: .5), borderRadius: BorderRadius.circular(15)),
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[Flexible(child: Text(data.keys.elementAt(index), style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: pink))), const SizedBox(height: 10)],
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        const Icon(Bootstrap.chevron_right, size: 15, color: pink),
+                      ],
                     ),
-                    separatorBuilder: (BuildContext context, int index) => Container(width: MediaQuery.sizeOf(context).width, height: .5, color: grey, margin: const EdgeInsets.symmetric(vertical: 8)),
-                    itemCount: data.length,
-                  );
-                } else {
-                  return const Empty();
-                }
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const LoadingScreen();
-              } else {
-                return RedScreenOfDeath(error: snapshot.error.toString());
-              }
-            }),
+                  ),
+                ),
+                separatorBuilder: (BuildContext context, int index) => Container(width: MediaQuery.sizeOf(context).width, height: .5, color: grey, margin: const EdgeInsets.symmetric(vertical: 8)),
+                itemCount: data.length,
+              );
+            } else {
+              return const Empty();
+            }
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingScreen();
+          } else {
+            return RedScreenOfDeath(error: snapshot.error.toString());
+          }
+        },
       ),
     );
   }
---
+++ lib/views/lister.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Lister extends StatelessWidget {
+  const Lister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ,);
+  }
+}
